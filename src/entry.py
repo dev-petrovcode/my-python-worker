aa76d4ddbd3dc4665dd2a5b26051e98df8053a7c
dev-petrovcode <mspkzain@example.com>
2026-01-06 04:08:57 +0530

    added react frontent

--- src/entry.py
+++ src/entry.py
@@ -1,5 +1,6 @@
 from workers import Response, WorkerEntrypoint
-from submodule import get_hello_message
-class Default(WorkerEntrypoint): 
-    async def fetch(self, request):
-        return Response(get_hello_message())
+
+class Default(WorkerEntrypoint):
+    async def fetch(self, request, env, ctx):
+        return await env.ASSETS.fetch(request) 
+        
